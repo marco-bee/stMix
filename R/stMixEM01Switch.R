2ecdf74b04a240673a88c9066e877be66a466c86
marco-bee <marco.bee@unitn.it>
2026-02-23 11:08:20 +0100

    Test with switching EM

--- R/stMixEM01Switch.R
+++ R/stMixEM01Switch.R
@@ -13,7 +13,6 @@
 #' @return A list with the following elements is returned:
 #' "p" = estimated value of p,
 #' "post" = posterior probabilities of all observations,
-#' "gamma1" = estimated value of \eqn{\gamma_1},
 #' "mu1" = estimated value of \eqn{\mu_1},
 #' "sigma1 " = estimated value of \eqn{\sigma_1},
 #' "nu1" = estimated value of \eqn{\nu_1},
@@ -30,7 +29,7 @@
 #'
 #' @importFrom Rdpack reprompt
 
-stMixEMSwitch <- function(Y,p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2,nustar,maxiter)
+stMixEM01Switch <- function(Y,p1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2,nustar,maxiter)
 {
   p2 <- 1 - p1
   n <- length(Y)
@@ -42,17 +41,17 @@ stMixEMSwitch <- function(Y,p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2,nusta
 
   while (2>1)
   {
-    parold <- c(p1,gamma1,mu1,sigma1,gamma2,mu2,sigma2)
+    parold <- c(p1,mu1,sigma1,gamma2,mu2,sigma2)
 
     # evaluate component densities and mixture density
 
     if (nu1<nustar)
     {
-      f1 <- dfst(Y,gamma1,mu1,sigma1,nu1)
+      f1 <- dfst(Y,1,mu1,sigma1,nu1)
     }
     if (nu1>=nustar)
     {
-      f1 <- dsnorm(Y,gamma1,mu1,sigma1)
+      f1 <- dsnorm(Y,1,mu1,sigma1)
     }
     if (nu2<nustar)
     {
@@ -81,30 +80,24 @@ stMixEMSwitch <- function(Y,p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2,nusta
 
     if (nu1<nustar)
     {
-      f1 <- dfst(Y,gamma1,mu1,sigma1,nu1)
-      # res1 <- optim(c(log(gamma1),mu1,log(sigma1),pmin(log(nu1-2),1.7e+307)),
-      #               llstWei, gr = NULL, control = list(fnscale = -1), Y, post1)
-      res1 <- optim(c(log(gamma1),mu1,log(sigma1),log(nu1)),
-                    llstWei, gr = NULL, control = list(fnscale = -1), Y, post1)
-      gamma1 <- exp(res1$par[1])
-      mu1 <- res1$par[2]
-      sigma1 <- exp(res1$par[3])
-      nu1 <- exp(res1$par[4]) # +2
+      f1 <- dfst(Y,1,mu1,sigma1,nu1)
+      res1 <- optim(c(mu1,log(sigma1),log(nu1)),
+                    llstWei0, gr = NULL, control = list(fnscale = -1), Y, post1)
+      mu1 <- res1$par[1]
+      sigma1 <- exp(res1$par[2])
+      nu1 <- exp(res1$par[3]) # +2
     }
     if (nu1>=nustar)
     {
-      f1 <- dsnorm(Y,gamma1,mu1,sigma1)
-      res1 <- optim(c(log(gamma1),mu1,log(sigma1)),
-                    llsnormWei, gr = NULL, control = list(fnscale = -1), Y, post1)
-      gamma1 <- exp(res1$par[1])
-      mu1 <- res1$par[2]
-      sigma1 <- exp(res1$par[3])
+      f1 <- dsnorm(Y,1,mu1,sigma1)
+      res1 <- optim(c(mu1,log(sigma1)),
+                    llsnormWei0, gr = NULL, control = list(fnscale = -1), Y, post1)
+      mu1 <- res1$par[1]
+      sigma1 <- exp(res1$par[2])
     }
     if (nu2<nustar)
     {
       f2 <- dfst(Y,gamma2,mu2,sigma2,nu2)
-      # res2 <- optim(c(log(gamma2),mu2,log(sigma2),pmin(log(nu2-2),1.7e+307)),
-      #               llstWei, gr = NULL, control = list(fnscale = -1), Y, post2)
       res2 <- optim(c(log(gamma2),mu2,log(sigma2),log(nu2)),
                     llstWei, gr = NULL, control = list(fnscale = -1), Y, post2)
       gamma2 <- exp(res2$par[1])
@@ -121,34 +114,34 @@ stMixEMSwitch <- function(Y,p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2,nusta
       mu2 <- res2$par[2]
       sigma2 <- exp(res2$par[3])
     }
-    loglik <- sum(log(dfstMix(Y,p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2)))
+    loglik <- sum(log(dfstMix(Y,p1,1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2)))
     
     # check convergence
 
-    diffpar <- parold - c(p1,gamma1,mu1,sigma1,gamma2,mu2,sigma2)
+    diffpar <- parold - c(p1,mu1,sigma1,gamma2,mu2,sigma2)
     change <- max(abs(diffpar))
     if (nit > maxiter || change < epsilon)
       break
     nit <- nit + 1
     if (nu1>=nustar)
     {
-      results <- list(p=p1,post=post1,gamma1=gamma1,mu1=mu1,sigma1=sigma1,nu1=Inf,
+      results <- list(p=p1,post=post1,mu1=mu1,sigma1=sigma1,nu1=Inf,
      gamma2=gamma2,mu2=mu2,sigma2=sigma2,nu2=nu2,nit=nit,loglik=loglik)
     }
     if (nu2>=nustar)
     {
-      results <- list(p=p1,post=post1,gamma1=gamma1,mu1=mu1,sigma1=sigma1,nu1=nu1,
-         gamma2=gamma2,mu2=mu2,sigma2=sigma2,nu2=Inf,nit=nit,loglik=loglik)
+      results <- list(p=p1,post=post1,mu1=mu1,sigma1=sigma1,nu1=nu1,
+      gamma2=gamma2,mu2=mu2,sigma2=sigma2,nu2=Inf,nit=nit,loglik=loglik)
     }
     if (nu1>=nustar & nu2>=nustar)
     {
-      results <- list(p=p1,post=post1,gamma1=gamma1,mu1=mu1,sigma1=sigma1,nu1=Inf,
-        gamma2=gamma2,mu2=mu2,sigma2=sigma2,nu2=Inf,nit=nit,loglik=loglik)
+      results <- list(p=p1,post=post1,mu1=mu1,sigma1=sigma1,nu1=Inf,
+    gamma2=gamma2,mu2=mu2,sigma2=sigma2,nu2=Inf,nit=nit,loglik=loglik)
     }
     if (nu1<nustar & nu2<nustar)
     {
-      results <- list(p=p1,post=post1,gamma1=gamma1,mu1=mu1,sigma1=sigma1,nu1=nu1,
-      gamma2=gamma2,mu2=mu2,sigma2=sigma2,nu2=nu2,nit=nit,loglik=loglik)
+      results <- list(p=p1,post=post1,mu1=mu1,sigma1=sigma1,nu1=nu1,
+       gamma2=gamma2,mu2=mu2,sigma2=sigma2,nu2=nu2,nit=nit,loglik=loglik)
     }
   }
   return(results)
