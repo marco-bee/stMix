4022ecbaac653f528d166d50b109fef90be8adda
marco-bee <marco.bee@unitn.it>
2026-03-29 20:35:22 +0200

    Implemented estimation with fixed gamma1 and/or gamma2

--- R/llstWei.R
+++ R/llstWei.R
@@ -18,10 +18,20 @@
 #' @importFrom Rdpack reprompt
 llstWei <- function(x,y,post)
 {
-  gammap <- exp(x[1])
-  mu <- x[2]
-  sigma <- exp(x[3])
-  nu <- exp(x[4])
+  if (length(x)==3)
+  {
+    gammap <- 1
+    mu <- x[1]
+    sigma <- exp(x[2])
+    nu <- exp(x[3])
+  }
+  if (length(x)==4)
+  {
+    gammap <- exp(x[1])
+    mu <- x[2]
+    sigma <- exp(x[3])
+    nu <- exp(x[4])
+  }
   ll <- dfst(y,gammap,mu,sigma,nu)
   llik <- sum(log(ll)*post)
   return(llik)
--- R/stMixEM.R
+++ R/stMixEM.R
@@ -22,6 +22,8 @@
 #' "nu2" = estimated value of \eqn{\nu_2},
 #' "nit" = number of iterations,
 #' "loglik" = maximized log-likelihood.
+#' @details If the initial value of \eqn{\gamma_1} and/or \eqn{\gamma_2} is
+#' NULL, the corresponding parameter is not estimated, but set equal to 1.
 #' @export
 #' @examples
 #' y <- rfstMix(100,.5,1.2,1.3,0.5,3,.8,1,1,7)
@@ -30,64 +32,216 @@
 #'
 #' @importFrom Rdpack reprompt
 
-stMixEM <- function(Y,p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2,cap=30,maxiter)
+stMixEM <- function(Y,p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,
+                    sigma2,nu2,cap=30,maxiter)
 {
   p2 <- 1 - p1
   n <- length(Y)
   nit <- 1
   epsilon <- 1.e-10
   change <- 500
-
+  
   # start iterations
-
+  
   while (2>1)
   {
-    parold <- c(p1,gamma1,mu1,sigma1,gamma2,mu2,sigma2)
-
-    # evaluate component densities and mixture density
-
-    f1 <- dfst(Y,gamma1,mu1,sigma1,nu1)
-    f2 <- dfst(Y,gamma2,mu2,sigma2,nu2)
-    f <- p1 * f1 + p2 * f2
-    loglik <- sum(log(f))
-
-    # update posterior probabilities: E step
-
-    post1 <- p1*f1 / f
-    post2 <- p2*f2 / f
-    post <- cbind(post1, post2)
-
-    # update prior probabilities: M step
-
-    prior <- colMeans(post)
-    p1 <- as.double(prior[1])
-    p2 <- as.double(prior[2])
-
-    # update means: M-step for means
-
-    res1 <- optim(c(log(gamma1),mu1,log(sigma1),pmin(log(nu1),cap)),
-                  llstWei, gr = NULL, control = list(fnscale = -1), Y, post1)
-    gamma1 <- exp(res1$par[1])
-    mu1 <- res1$par[2]
-    sigma1 <- exp(res1$par[3])
-    nu1 <- pmin(exp(res1$par[4]),cap)
-    res2 <- optim(c(log(gamma2),mu2,log(sigma2),pmin(log(nu2),cap)),
-          llstWei, gr = NULL, control = list(fnscale = -1), Y, post2)
-    gamma2 <- exp(res2$par[1])
-    mu2 <- res2$par[2]
-    sigma2 <- exp(res2$par[3])
-    nu2 <- pmin(exp(res2$par[4]),cap)
-    loglik <- sum(log(dfstMix(Y,p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2)))
-    
-    # check convergence
-
-    diffpar <- parold - c(p1,gamma1,mu1,sigma1,gamma2,mu2,sigma2)
-    change <- max(abs(diffpar))
-    if (nit > maxiter || change < epsilon)
-      break
-    nit <- nit + 1
-    estPars <- c(p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2)
-    results <- list(estPars=estPars,post=post1,nit=nit,loglik=loglik)
+    if (!is.null(gamma1) & !is.null(gamma2))
+    {
+      parold <- c(p1,gamma1,mu1,sigma1,gamma2,mu2,sigma2)
+      
+      # evaluate component densities and mixture density
+      
+      f1 <- dfst(Y,gamma1,mu1,sigma1,nu1)
+      f2 <- dfst(Y,gamma2,mu2,sigma2,nu2)
+      f <- p1 * f1 + p2 * f2
+      loglik <- sum(log(f))
+      
+      # update posterior probabilities: E step
+      
+      post1 <- p1*f1 / f
+      post2 <- p2*f2 / f
+      post <- cbind(post1, post2)
+      
+      # update prior probabilities: M step
+      
+      prior <- colMeans(post)
+      p1 <- as.double(prior[1])
+      p2 <- as.double(prior[2])
+      
+      # update means: M-step for means
+      
+      res1 <- optim(c(log(gamma1),mu1,log(sigma1),pmin(log(nu1),cap)),
+                    llstWei, gr = NULL, control = list(fnscale = -1), Y, post1)
+      gamma1 <- exp(res1$par[1])
+      mu1 <- res1$par[2]
+      sigma1 <- exp(res1$par[3])
+      nu1 <- pmin(exp(res1$par[4]),cap)
+      res2 <- optim(c(log(gamma2),mu2,log(sigma2),pmin(log(nu2),cap)),
+                    llstWei, gr = NULL, control = list(fnscale = -1), Y, post2)
+      gamma2 <- exp(res2$par[1])
+      mu2 <- res2$par[2]
+      sigma2 <- exp(res2$par[3])
+      nu2 <- pmin(exp(res2$par[4]),cap)
+      loglik <- sum(log(dfstMix(Y,p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2)))
+      
+      # check convergence
+      
+      diffpar <- parold - c(p1,gamma1,mu1,sigma1,gamma2,mu2,sigma2)
+      change <- max(abs(diffpar))
+      if (nit > maxiter || change < epsilon)
+        break
+      nit <- nit + 1
+      estPars <- c(p1,gamma1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2)
+      results <- list(estPars=estPars,post=post1,nit=nit,loglik=loglik)
+    }
+    if (is.null(gamma1) & !is.null(gamma2))
+    {
+      gamma1_used <- 1
+      parold <- c(p1,mu1,sigma1,gamma2,mu2,sigma2)
+      
+      # evaluate component densities and mixture density
+      
+      f1 <- dfst(Y,gamma1_used,mu1,sigma1,nu1)
+      f2 <- dfst(Y,gamma2,mu2,sigma2,nu2)
+      f <- p1 * f1 + p2 * f2
+      loglik <- sum(log(f))
+      
+      # update posterior probabilities: E step
+      
+      post1 <- p1*f1 / f
+      post2 <- p2*f2 / f
+      post <- cbind(post1, post2)
+      
+      # update prior probabilities: M step
+      
+      prior <- colMeans(post)
+      p1 <- as.double(prior[1])
+      p2 <- as.double(prior[2])
+      
+      # update means: M-step for means
+      
+      res1 <- optim(c(mu1,log(sigma1),pmin(log(nu1),cap)),
+                    llstWei, gr = NULL, control = list(fnscale = -1), Y, post1)
+      # gamma1 <- exp(res1$par[1])
+      mu1 <- res1$par[1]
+      sigma1 <- exp(res1$par[2])
+      nu1 <- pmin(exp(res1$par[3]),cap)
+      res2 <- optim(c(log(gamma2),mu2,log(sigma2),pmin(log(nu2),cap)),
+                    llstWei, gr = NULL, control = list(fnscale = -1), Y, post2)
+      gamma2 <- exp(res2$par[1])
+      mu2 <- res2$par[2]
+      sigma2 <- exp(res2$par[3])
+      nu2 <- pmin(exp(res2$par[4]),cap)
+      loglik <- sum(log(dfstMix(Y,p1,gamma1_used,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2)))
+      
+      # check convergence
+      
+      diffpar <- parold - c(p1,mu1,sigma1,gamma2,mu2,sigma2)
+      change <- max(abs(diffpar))
+      if (nit > maxiter || change < epsilon)
+        break
+      nit <- nit + 1
+      estPars <- c(p1,mu1,sigma1,nu1,gamma2,mu2,sigma2,nu2)
+      results <- list(estPars=estPars,post=post1,nit=nit,loglik=loglik)
+    }
+    if (!is.null(gamma1) & is.null(gamma2))
+    {
+      gamma2_used <- 1
+      parold <- c(p1,gamma1,mu1,sigma1,mu2,sigma2)
+      
+      # evaluate component densities and mixture density
+      
+      f1 <- dfst(Y,gamma1,mu1,sigma1,nu1)
+      f2 <- dfst(Y,gamma2_used,mu2,sigma2,nu2)
+      f <- p1 * f1 + p2 * f2
+      loglik <- sum(log(f))
+      
+      # update posterior probabilities: E step
+      
+      post1 <- p1*f1 / f
+      post2 <- p2*f2 / f
+      post <- cbind(post1, post2)
+      
+      # update prior probabilities: M step
+      
+      prior <- colMeans(post)
+      p1 <- as.double(prior[1])
+      p2 <- as.double(prior[2])
+      
+      # update means: M-step for means
+      
+      res1 <- optim(c(log(gamma1),mu1,log(sigma1),pmin(log(nu1),cap)),
+                    llstWei, gr = NULL, control = list(fnscale = -1), Y, post1)
+      gamma1 <- exp(res1$par[1])
+      mu1 <- res1$par[2]
+      sigma1 <- exp(res1$par[3])
+      nu1 <- pmin(exp(res1$par[4]),cap)
+      res2 <- optim(c(mu2,log(sigma2),pmin(log(nu2),cap)),
+                    llstWei, gr = NULL, control = list(fnscale = -1), Y, post2)
+      mu2 <- res2$par[1]
+      sigma2 <- exp(res2$par[2])
+      nu2 <- pmin(exp(res2$par[3]),cap)
+      loglik <- sum(log(dfstMix(Y,p1,gamma1,mu1,sigma1,nu1,gamma2_used,mu2,sigma2,nu2)))
+      
+      # check convergence
+      
+      diffpar <- parold - c(p1,gamma1,mu1,sigma1,mu2,sigma2)
+      change <- max(abs(diffpar))
+      if (nit > maxiter || change < epsilon)
+        break
+      nit <- nit + 1
+      estPars <- c(p1,gamma1,mu1,sigma1,nu1,mu2,sigma2,nu2)
+      results <- list(estPars=estPars,post=post1,nit=nit,loglik=loglik)
+    }
+    if (is.null(gamma1) & is.null(gamma2))
+    {
+      gamma1_used <- 1
+      gamma2_used <- 1
+      parold <- c(p1,mu1,sigma1,mu2,sigma2)
+      
+      # evaluate component densities and mixture density
+      
+      f1 <- dfst(Y,gamma1_used,mu1,sigma1,nu1)
+      f2 <- dfst(Y,gamma2_used,mu2,sigma2,nu2)
+      f <- p1 * f1 + p2 * f2
+      loglik <- sum(log(f))
+      
+      # update posterior probabilities: E step
+      
+      post1 <- p1*f1 / f
+      post2 <- p2*f2 / f
+      post <- cbind(post1, post2)
+      
+      # update prior probabilities: M step
+      
+      prior <- colMeans(post)
+      p1 <- as.double(prior[1])
+      p2 <- as.double(prior[2])
+      
+      # update means: M-step for means
+      
+      res1 <- optim(c(mu1,log(sigma1),pmin(log(nu1),cap)),
+                    llstWei, gr = NULL, control = list(fnscale = -1), Y, post1)
+      mu1 <- res1$par[1]
+      sigma1 <- exp(res1$par[2])
+      nu1 <- pmin(exp(res1$par[3]),cap)
+      res2 <- optim(c(mu2,log(sigma2),pmin(log(nu2),cap)),
+                    llstWei, gr = NULL, control = list(fnscale = -1), Y, post2)
+      mu2 <- res2$par[1]
+      sigma2 <- exp(res2$par[2])
+      nu2 <- pmin(exp(res2$par[3]),cap)
+      loglik <- sum(log(dfstMix(Y,p1,gamma1_used,mu1,sigma1,nu1,gamma2_used,mu2,sigma2,nu2)))
+      
+      # check convergence
+      
+      diffpar <- parold - c(p1,mu1,sigma1,mu2,sigma2)
+      change <- max(abs(diffpar))
+      if (nit > maxiter || change < epsilon)
+        break
+      nit <- nit + 1
+      estPars <- c(p1,gamma1,mu1,sigma1,nu1,mu2,sigma2,nu2)
+      results <- list(estPars=estPars,post=post1,nit=nit,loglik=loglik)
+    }
   }
   return(results)
 }
