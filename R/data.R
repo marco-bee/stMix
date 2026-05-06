31a03997498ae745f8deb649eb268c7ae65fd326
marco-bee <marco.bee@unitn.it>
2026-05-06 20:38:43 +0200

    Added distortion data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Shiller's distortion data
+#'
+#' A dataset containing the monthly distortion of the S&P 500 index
+#' from January 1871 until August 2025. The distortion is defined as the
+#' log difference between the real stock market index and its real fundamental
+#' value; see Shiller (2015).
+#'
+#' @format A numerical vector with 1857 rows and 1 column.
+#' @source The data needed for the computation of the S&P 500 distortion
+#' can be downloaded from \url{http://www.econ.yale.edu/~shiller/data.htm}
+"distortion"
